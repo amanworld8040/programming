2489ae9d8269a329acc2719dd5ea24ddc29f8a43
Aman Rajak <111343496+amanworld8040@users.noreply.github.com>
2024-08-07 21:17:17 +0530

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,46 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Main {
+
+   static class stack {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        public  boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        //push
+        public  void push(int data) {
+            list.add(data);
+        }
+
+        //pop
+        public  int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = list.remove(list.size() - 1);
+            return top;
+        }
+        public  int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+        }
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+       stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }}
